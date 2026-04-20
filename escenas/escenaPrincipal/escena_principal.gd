1123735e597800a4b8829a256ac55d774fe4f80c
Fran <franmiceli08@gmail.com>
2026-04-20 11:20:49 -0300

    solucion: colision pj con plataforma transpasable. cambios identacion

--- escenas/escenaPrincipal/escena_principal.gd
+++ escenas/escenaPrincipal/escena_principal.gd
@@ -8,23 +8,20 @@ var _nivelActualInstancia: Node
 func _ready() -> void:
 	_crearNivel(_nivelActual)
 
-
 func _crearNivel(numeroNivel :int):
 	_nivelActualInstancia =niveles[numeroNivel-1].instantiate()
 	add_child(_nivelActualInstancia)
-	
-						#:= es para decir que la variable sea del tipo del que le estamos metiendo
+			#:= es para decir que la variable sea del tipo del que le estamos metiendo
 	var hijos := _nivelActualInstancia.get_children() #agarra todos los hijos personajes
 	for i in hijos.size():
 		if hijos[i].is_in_group("personajes"):
 			hijos[i].muertePersonaje.connect(_reiniciarNivel)
 			break
-			
 
 func _eliminarNivel():
 	_nivelActualInstancia.queue_free() #queue_free es para eliminar nodo y todos los hijos 
-																			#(añadir este nodo a la cola de nodos que sera eliminados
+#( queue_free agrega este nodo a la cola de nodos que sera eliminados
 
 func _reiniciarNivel():
 	_eliminarNivel()
-	_crearNivel.call_deferred(_nivelActual) #Calls the method in deferred mode, at the end of the current frame
+	_crearNivel.call_deferred(_nivelActual) #Calls the method in deferred mode, at the end of the current framee
