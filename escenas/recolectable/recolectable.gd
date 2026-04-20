b031acc7fe29c2d41dffec0e957998df097647c5
Fran <franmiceli08@gmail.com>
2026-04-20 17:48:25 -0300

    pantalla final

--- escenas/recolectable/recolectable.gd
+++ escenas/recolectable/recolectable.gd
@@ -6,7 +6,15 @@ var contenedorRecolectables: ContenedorRecolectables
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	area2d.body_entered.connect(_recogida)
+	_animar()
+
 
 func _recogida(_body):
 	contenedorRecolectables.recolectableAgarrado()
 	queue_free()
+
+func _animar():
+	var tween: Tween=create_tween()
+	tween.set_loops(0)#con 0 es infinito
+	tween.tween_property(self,"position:y", position.y - 2, 1)
+	tween.tween_property(self,"position:y", position.y + 2, 1)
