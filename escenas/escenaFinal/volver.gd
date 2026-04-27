4b362ad72625979dd578e2b8f672421925e5f08c
Fran <franmiceli08@gmail.com>
2026-04-27 05:53:56 -0300

    juego terminado, faltarian adiciones como efectos de sonido, le preguntaré al profe que opina

---
+++ escenas/escenaFinal/volver.gd
@@ -0,0 +1,10 @@
+extends Button
+
+@export var menuInicial: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pressed.connect(_volver)
+
+func _volver():
+	get_tree().change_scene_to_packed(menuInicial)
